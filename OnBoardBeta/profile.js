fd1ca88a322390c17a663fb3663e4b43307bdd5b
Harunzy <Harunzy55@gmail.com>
2025-11-06 15:03:12 +0000

    add update to the profile page

--- OnBoardBeta/profile.js
+++ OnBoardBeta/profile.js
@@ -1,32 +1,30 @@
 const form = document.querySelector('form');
+const imageInput = document.getElementById('image-input');
+const imagePreview = document.getElementById('image-preview');
+
+//upload profile picture
+imageInput.addEventListener('change', (e) => {
+  const file = imageInput.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    imagePreview.src = reader.result;
+  };
+  reader.readAsDataURL(file);
+});
+
+//take input from user
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const profileArray = [];
-
-  // Access values employee entered
-  const profilePic = document.querySelector('.profile-picture').value;
-  const fullName = document.querySelector('.fullName').value;
-  const email = document.querySelector('.email').value;
-  const department = document.querySelector('#department').value;
-  const jobTitle = document.querySelector('.jobTitle').value;
-  const startDate = document.querySelector('.startDate').value;
-  const employmentId = document.querySelector('.employmentId').value;
-
-  const employeePicDiv = document.querySelector('.user-avator');
-  const employeePic = document.createElement('img');
-  employeePic.className = 'employeePicture';
-  employeePic.src = `image/${profilePic}`;
-  employeePicDiv.appendChild(employeePic);
-
-  // push to profileArray
-  profileArray.push(profilePic);
-  profileArray.push(fullName);
-  profileArray.push(email);
-  profileArray.push(department);
-  profileArray.push(jobTitle);
-  profileArray.push(startDate);
-  profileArray.push(employmentId);
+  const profileArray = [
+    document.querySelector('.fullName').value,
+    document.querySelector('.email').value,
+    document.querySelector('.department').value,
+    document.querySelector('.jobTitle').value,
+    +document.querySelector('.startDate').value,
+    document.querySelector('.employmentId').value,
+  ];
 
   console.log(profileArray);
 });
